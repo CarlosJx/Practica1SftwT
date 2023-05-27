16b10853464ac458637b91de050ffef56fbc653d
CarlosJx <ospinacifuentescarlosjose@gmail.com>
2023-05-26 20:35:46 -0500

    TS

--- Backend/API/src/index.ts
+++ Backend/API/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import personaRouter from './routes/persona.router';
 
 const app = express();
 const port = 3000;
 
+app.use (cors());
 app.use(express.json());
 app.use('/api/persona',personaRouter);
 
